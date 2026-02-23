953e6088adb25b48eda25d4608dfa94af14980db
karmirarev <maritamovsisyan@gmail.com>
2026-02-23 12:50:03 +0400

    - added a background

--- core/src/main/java/com/your/mom/Main.java
+++ core/src/main/java/com/your/mom/Main.java
@@ -1,32 +1,40 @@
 package com.your.mom;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture background;
+    private FitViewport viewport;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        viewport = new FitViewport(1280, 800);
+        background = new Texture("background.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(Color.DARK_GRAY);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(background, 0, 0);
         batch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 }
